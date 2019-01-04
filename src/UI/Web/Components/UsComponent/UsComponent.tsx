16299510d81d480c774e90c9f8f5e0434a161366
Mayank Tripathi <mayank.tripathi@shl.com>
2019-01-04 11:31:46 +0530

    Minor CSS changes

--- src/UI/Web/Components/UsComponent/UsComponent.tsx
+++ src/UI/Web/Components/UsComponent/UsComponent.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { observer } from "mobx-react";
 // import { PageStore } from "UI/Stores/PageStore";
 
+const _topDesign = require("../../Styles/images/top-design.png"); // tslint:disable-line:no-require-imports no-var-requires
+
 @observer
 export default class UsComponent extends React.Component<{}> {
 
@@ -14,8 +16,11 @@ export default class UsComponent extends React.Component<{}> {
 
   public render() {
     return (
-      <div className={`d-flex flex-column _usPage`}>
-        Us comes here
+      <div className={`d-flex flex-column _usPage col-12`}>
+        <div className="text-center py-3">
+          <img className="top-design" src={_topDesign} />
+        </div>
+        <h1 className="mt-2 text-center">Us<span>:</span> Today <span>&amp;</span> Forever</h1>
       </div>
     );
   }
